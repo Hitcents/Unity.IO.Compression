67f8a78702e69e4eb9e2e54af91f0cabf514ca0c
Jonathan Peppers <jonathan.peppers@gmail.com>
2015-03-03 16:24:44 -0600

    A couple more tests

--- Assets/Editor/Tests/GZipStreamTests.cs
+++ Assets/Editor/Tests/GZipStreamTests.cs
@@ -1,4 +1,5 @@
 ﻿using NUnit.Framework;
+using System;
 using System.IO;
 using Unity.IO.Compression;
 
@@ -50,6 +51,44 @@ public class GZipStreamTests : BaseStreamTests
         {
             string text = reader.ReadToEnd();
             Assert.IsTrue(text.Contains("\"gzipped\": true"), "Request was not compressed!");
+        }
+    }
+
+    [Test]
+    public void LeaveOpenTrue()
+    {
+        using (var memoryStream = new MemoryStream())
+        {
+            using (var gzipStream = new GZipStream(memoryStream, CompressionMode.Compress, true))
+            using (var writer = new StreamWriter(gzipStream))
+            {
+                writer.Write(Text);
+            }
+
+            using (var gzipStream = new GZipStream(memoryStream, CompressionMode.Compress, true))
+            using (var writer = new StreamWriter(gzipStream))
+            {
+                writer.Write(Text);
+            }
+        }
+    }
+
+    [Test, ExpectedException(typeof(ArgumentException))]
+    public void LeaveOpenFalse()
+    {
+        using (var memoryStream = new MemoryStream())
+        {
+            using (var gzipStream = new GZipStream(memoryStream, CompressionMode.Compress, false))
+            using (var writer = new StreamWriter(gzipStream))
+            {
+                writer.Write(Text);
+            }
+
+            using (var gzipStream = new GZipStream(memoryStream, CompressionMode.Compress, false))
+            using (var writer = new StreamWriter(gzipStream))
+            {
+                writer.Write(Text);
+            }
         }
     }
 }
