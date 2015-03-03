23b18afa8f3a57eaaa4ef8a04a062c0aa63d6cfa
Jonathan Peppers <jonathan.peppers@gmail.com>
2015-03-03 15:31:33 -0600

    Started adding test... Need to fix namespace

---
+++ Assets/Editor/Tests/GZipStreamTests.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+using System.IO.Compression;
+using NUnit.Framework;
+
+[TestFixture]
+public class GZipStreamTests 
+{
+    [Test]
+    public void Test()
+    {
+        using (var memoryStream = new MemoryStream())
+        using (var gzipStream = new GZipStream(memoryStream, CompressionLevel.Fastest))
+        using (var writer = new StreamWriter(gzipStream))
+        {
+
+        }
+    }
+}
