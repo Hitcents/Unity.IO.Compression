02b61083bd530b8b663aa4f2d58da4b04682d35b
Jonathan Peppers <jonathan.peppers@gmail.com>
2015-03-03 15:41:24 -0600

    Got tests running

--- Assets/Editor/Tests/GZipStreamTests.cs
+++ Assets/Editor/Tests/GZipStreamTests.cs
@@ -1,18 +1,60 @@
 ﻿using System.IO;
-using System.IO.Compression;
+using Unity.IO.Compression;
 using NUnit.Framework;
 
 [TestFixture]
 public class GZipStreamTests 
 {
+    private string Text = "This is example text that we might want to GZip because it is really long and weird. I would be embarrassed to read this. This is example text that we might want to GZip because it is really long and weird. I would be embarrassed to read this. This is example text that we might want to GZip because it is really long and weird. I would be embarrassed to read this. This is example text that we might want to GZip because it is really long and weird. I would be embarrassed to read this. This is example text that we might want to GZip because it is really long and weird. I would be embarrassed to read this. This is example text that we might want to GZip because it is really long and weird. I would be embarrassed to read this. This is example text that we might want to GZip because it is really long and weird. I would be embarrassed to read this. This is example text that we might want to GZip because it is really long and weird. I would be embarrassed to read this. This is example text that we might want to GZip because it is really long and weird. I would be embarrassed to read this. This is example text that we might want to GZip because it is really long and weird. I would be embarrassed to read this. This is example text that we might want to GZip because it is really long and weird. I would be embarrassed to read this. This is example text that we might want to GZip because it is really long and weird. I would be embarrassed to read this. This is example text that we might want to GZip because it is really long and weird. I would be embarrassed to read this. This is example text that we might want to GZip because it is really long and weird. I would be embarrassed to read this.";
+
     [Test]
-    public void Test()
+    public void GZipFastest()
     {
         using (var memoryStream = new MemoryStream())
-        using (var gzipStream = new GZipStream(memoryStream, CompressionLevel.Fastest))
-        using (var writer = new StreamWriter(gzipStream))
         {
+            using (var gzipStream = new GZipStream(memoryStream, CompressionLevel.Fastest))
+            using (var writer = new StreamWriter(gzipStream))
+            {
+                writer.Write(Text);
+            }
+            memoryStream.Flush();
 
+            var bytes = memoryStream.ToArray();
+            Assert.AreEqual(235, bytes.Length);
+        }
+    }
+
+    [Test]
+    public void GZipOptimal()
+    {
+        using (var memoryStream = new MemoryStream())
+        {
+            using (var gzipStream = new GZipStream(memoryStream, CompressionLevel.Optimal))
+            using (var writer = new StreamWriter(gzipStream))
+            {
+                writer.Write(Text);
+            }
+            memoryStream.Flush();
+
+            var bytes = memoryStream.ToArray();
+            Assert.AreEqual(235, bytes.Length);
+        }
+    }
+
+    [Test]
+    public void GZipNoCompression()
+    {
+        using (var memoryStream = new MemoryStream())
+        {
+            using (var gzipStream = new GZipStream(memoryStream, CompressionLevel.NoCompression))
+            using (var writer = new StreamWriter(gzipStream))
+            {
+                writer.Write(Text);
+            }
+            memoryStream.Flush();
+
+            var bytes = memoryStream.ToArray();
+            Assert.AreEqual(235, bytes.Length);
         }
     }
 }
